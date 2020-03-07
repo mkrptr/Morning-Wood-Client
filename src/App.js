ad89675d08d941a50ed56d28403124ea786ea2e4
Mihail <shabunin.mihail1983@gmail.com>
2020-03-07 23:44:29 +0400

    Setup dev scripts. Make basic index page.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
+import { APP_LOAD, REDIRECT } from './action_types';
+import Header from './components/header';
+import Home from './components/home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStateToProps = state => {
+  return {
+    appLoaded: state.common.appLoaded,
+    appName: state.common.appName,
+    currentUser: state.common.currentUser,
+    redirectTo: state.common.redirectTo
+}};
+
+const mapDispatchToProps = dispatch => ({
+    onLoad: (payload, token) =>
+        dispatch({ type: APP_LOAD, payload, token, skipTracking: true }),
+    onRedirect: () =>
+        dispatch({ type: REDIRECT })
+});
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header appName={this.props.appName} />
+                <Switch>
+                </Switch>
+            </div>
+        );
+    }  
 }
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import { ConnectedRouter } from 'connected-react-router';
+
 import './index.css';
+import store, { history } from './store';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log(store);
+console.log(history);
+console.log(App);
+console.log(ConnectedRouter);
+console.log(Provider);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render((
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route path="/" component={App}/>
+            </Switch>
+        </ConnectedRouter>
+    </Provider>
+    ), 
+    document.getElementById('root'));
+
