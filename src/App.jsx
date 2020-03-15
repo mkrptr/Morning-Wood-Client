511325ec52506ee530ca12180aa8c174a57836d2
Alexey <afanasjew.aleksey2020@gmail.com>
2020-03-15 23:04:37 +0400

    Refactor modules

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { inject, observer } from 'mobx-react';
 import './App.css';
-import Header from './components/header/header';
-import Home from './components/home/home';
-import Footer from './components/footer/footer';
-import Login from './components/login/login';
-import Lot from './components/lot/lot';
+import Header from './common_components/header/header';
+import Home from './views/home/home';
+import Footer from './common_components/footer/footer';
+import Login from './views/login/login';
+import Lot from './common_components/lot/lot';
+import Catalog from './views/catalog/catalog';
+
 
 @inject('authStore')
 @withRouter
@@ -16,7 +18,6 @@ import Lot from './components/lot/lot';
 class App extends React.Component {
     render() {
         const { appName, authStore } = this.props;
-        console.log(this.props);
         return (
             <div>
                 <Header
@@ -27,6 +28,7 @@ class App extends React.Component {
                     <Route exact path="/" component={Home} />
                     <Route path="/login" component={Login} />
                     <Route path="/lots/:id" component={Lot} />
+                    <Route path="/catalog" component={Catalog} />
                 </Switch>
                 <Footer />
             </div>
