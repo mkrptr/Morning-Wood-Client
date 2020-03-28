fc1c93d9c3f27e57446cafb916dfe306718013bb
Alexey <afanasjew.aleksey2020@gmail.com>
2020-03-28 22:29:09 +0400

    Make catalog fully working

--- src/views/catalog/catalog.jsx
+++ src/views/catalog/catalog.jsx
@@ -8,37 +8,62 @@ import Lots from './components/lots/lots';
 
 const Catalog = () => {
     const [lots, setLots] = useState([]);
+    const [lotsLoaded, setLotsLoaded] = useState(false);
+
     useEffect(() => {
         const getLots = async () => {
             try {
                 const lotsRes = await fetch(`${API_URL}/lots`);
                 setLots(await lotsRes.json());
             } catch (e) {
-                console.error(e);
                 const lotsStub = [
                     {
+                        id: 1,
                         name: 'Nigga wood',
                         price: 200,
                         description: '',
                     },
                     {
+                        id: 2,
                         name: 'Red wood',
                         price: 1000,
                         description: '',
                     },
                     {
+                        id: 3,
                         name: 'Black wood',
                         price: 60,
                         description: '',
                     },
+                    {
+                        id: 4,
+                        name: 'Mystery wood',
+                        price: 900,
+                        description: '',
+                    },
+                    {
+                        id: 5,
+                        name: 'Buff wood',
+                        price: 650,
+                        description: '',
+                    },
                 ];
-                setLots(Array.from({ length: 5 }, () => lotsStub).flat());
+                setLots(lotsStub);
+                setLotsLoaded(true);
             }
         };
-        getLots();
-    }, [setLots]);
+        if (!lotsLoaded) {
+            getLots();
+        }
+        console.log('catalog: effect');
+        console.log(lots);
+    }, [setLots, lotsLoaded, setLotsLoaded, lots]);
 
-    const onLotsUpdate = (newLots) => setLots(newLots);
+    const onLotsUpdate = (newLots) => {
+        console.log('catalog: update');
+        console.log(newLots);
+        setLots(newLots);
+    };
 
 
     return (
@@ -48,7 +73,7 @@ const Catalog = () => {
             </div>
             <div className={styles.catalogBody}>
                 <div className={styles.filter}>
-                    <Filters lots={lots} onUpdate={onLotsUpdate} />
+                    <Filters lots={lots} setLots={onLotsUpdate} />
                 </div>
                 <div className={styles.lots}>
                     <Lots lots={lots} />
--- src/views/catalog/components/filters/filters.jsx
+++ src/views/catalog/components/filters/filters.jsx
@@ -1,89 +1,99 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
 import styles from './filters.module.css';
-import FilterButton from '../filter_button/filter_button';
 
 const FilterParameter = Object.freeze({
-    none: 0,
-    color: 1,
-    price: 2,
-    maker: 3,
+    PRICE_ASC: {
+        alias: 'price_asc',
+        humanReadable: 'price ascending',
+    },
+    PRICE_DESC: {
+        alias: 'price_desc',
+        humanReadable: 'price descending',
+    },
+    NAME: {
+        alias: 'name',
+        humanReadable: 'name',
+    },
 });
 
 const Filters = (props) => {
-    const { lots, onUpdate } = props;
+    const { lots, setLots } = props;
     const [filtersState, setFiltersState] = useState({
-        parameter: FilterParameter.none,
-        ascending: true,
+        parameter: FilterParameter.PRICE_ASC,
     });
 
-    useEffect(() => {
-        const filterLots = (old_lots) => {
-            switch (filtersState.parameter) {
-                case FilterParameter.none:
-                    break;
-                case FilterParameter.brand:
-
-            }
-        };
-    }, [lots]);
-
-    const toggle = (state, parameter) => {
-        if (state.parameter === parameter) {
-            setFiltersState({
-                ...state,
-                ascending: !state.ascending,
-            });
+    const filterLots = (oldLots, parameter) => {
+        switch (parameter.alias) {
+        case FilterParameter.PRICE_ASC.alias:
+            return oldLots.sort((a, b) => (
+                a.price > b.price
+            ));
+        case FilterParameter.PRICE_DESC.alias:
+            return oldLots.sort((a, b) => (
+                a.price < b.price
+            ));
+        case FilterParameter.NAME.alias:
+            return oldLots.sort((a, b) => (
+                a.name > b.name
+            ));
+        default:
+            console.error('invalid parameter');
+            return [];
         }
-        setFiltersState({
-            parameter,
-            ascending: !!state.filterState.ascending,
-        });
     };
 
+    const toggle = (event) => {
+        const parameter = Object.entries(FilterParameter)
+            .filter((parameterEntry) => parameterEntry[1].alias === event.target.value)
+            .map((parameterEntry) => parameterEntry[0])[0];
+        if (parameter === undefined) {
+            console.error('Filters/toggle: invalid parameter value');
+            return;
+        }
+        setFiltersState({
+            parameter: FilterParameter[parameter],
+        });
+        setLots(filterLots([...lots], FilterParameter[parameter]));
+    };
+    const options = Object.entries(FilterParameter)
+        .map((entry) => (
+            <option
+                key={entry[0]}
+                value={entry[1].alias}
+            >
+                {entry[1].humanReadable}
+            </option>
+        ));
     return (
         <div className={styles.filterWrapper}>
-            <div className={styles.parameterSortBlock}>
-                <FilterButton
-                    name="Brand"
-                    toggle={toggle(filtersState, FilterParameter.brand)}
-                />
-                <div className={styles.parameterSortButton}>
-                    <p className={styles.parameterSortName}>Color</p>
-                    <svg
-                        className={styles.parameterSortArrow}
-                        role="presentation"
-                        viewBox="0 0 21 11"
-                    >
-                        <polyline
-                            fill="none"
-                            stroke="currentColor"
-                            points="0.5 0.5 10.5 10.5 20.5 0.5"
-                            strokeWidth="1.25"
-                        />
-                    </svg>
-                </div>
-                <div className={styles.parameterSortButton}>
-                    <p className={styles.parameterSortName}>Maker</p>
-                    <svg
-                        className={styles.parameterSortArrow}
-                        role="presentation"
-                        viewBox="0 0 21 11"
-                    >
-                        <polyline
-                            fill="none"
-                            stroke="currentColor"
-                            points="0.5 0.5 10.5 10.5 20.5 0.5"
-                            strokeWidth="1.25"
-                        />
-                    </svg>
-                </div>
-            </div>
             <div className={styles.parameterSortButton}>
                 <p className={styles.parameterSortName}>
                     Sort by:
                 </p>
+                <select
+                    className={styles.parameterSortSelect}
+                    value={filtersState.parameter.alias}
+                    onChange={toggle}
+                >
+                    {options}
+                </select>
             </div>
         </div>
     );
 };
+
+Filters.propTypes = {
+    lots: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            price: PropTypes.number.isRequired,
+            description: PropTypes.string,
+        }).isRequired,
+    ).isRequired,
+    setLots: PropTypes.func.isRequired,
+};
+
 export default Filters;
