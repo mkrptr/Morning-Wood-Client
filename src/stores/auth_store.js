9bbb73855ad162011403027e116fe229d494afe3
Mihail <shabunin.mihail1983@gmail.com>
2020-03-29 12:48:24 +0400

    Add register view

--- src/stores/auth_store.js
+++ src/stores/auth_store.js
@@ -1,12 +1,14 @@
 import { observable, action } from 'mobx';
 import ApiService from '../services/api.service';
+import JwtService from '../services/jwt.service';
+import UserStore from './user_store';
 
 class AuthStore {
     @observable errors = [];
 
     @observable inProgress = false;
 
-    @observable isAuthenticated = false;
+    @observable isAuthenticated = !!JwtService.getToken();
 
     @action async login(login, password) {
         this.inProgress = true;
@@ -21,17 +23,45 @@ class AuthStore {
             this.errors.push({ body: 'Incorrect login or password' });
         }
         this.inProgress = false;
-        return success;
         try {
-            await ApiService.post('/login', {
+            const { user, token } = await ApiService.post('/login', {
                 login,
                 password,
             });
+            UserStore.setUser(user);
+            JwtService.saveToken(token);
         } catch (e) {
             this.errors.push({ body: 'Incorrect login or password' });
         } finally {
             this.inProgress = false;
         }
+    }
+
+    @action
+    async register(login, password, passwordConfirm) {
+        if (password !== passwordConfirm) {
+            this.errors.push({ body: 'Password don\'t match' });
+        }
+        this.inProgress = true;
+        try {
+            const newUser = await ApiService.post('/register', {
+                login,
+                password,
+                passwordConfirm,
+            });
+            this.isAuthenticated = true;
+        } catch (e) {
+            this.errors.push('');
+        } finally {
+            this.inProgress = false;
+        }
+    }
+
+    @action
+    logout() {
+        UserStore.purgeUser();
+        JwtService.purgeToken();
+        this.isAuthenticated = false;
     }
 }
 
