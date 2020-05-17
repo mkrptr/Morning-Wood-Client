82183b8b92b8b0713a9138d2a19b05a0ca0bc493
Mihail <shabunin.mihail1983@gmail.com>
2020-05-17 16:38:34 +0400

    Fix bug with empty localStorage item

--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -1,10 +1,16 @@
 /* eslint-disable no-undef */
 const TOKEN_NAME = 'mw_token';
 
-const JwtService = () => {
-    const getToken = () => window.localStorage.getItem(TOKEN_NAME);
-    const saveToken = (token) => window.localStorage.setItem(TOKEN_NAME, token);
-    const purgeToken = () => window.localStorage.removeItem(TOKEN_NAME);
+const JwtService = {
+    getToken () {
+        return window.localStorage.getItem(TOKEN_NAME);
+    },
+    saveToken(token) {
+        window.localStorage.setItem(TOKEN_NAME, token);
+    },
+    purgeToken() {
+        window.localStorage.removeItem(TOKEN_NAME);
+    }
 };
 
 export default JwtService;
